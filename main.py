139d842344be48288814a02666a00e751e3da207
Glen Dale Davis <glen.dale.davis@gmail.com>
2023-02-21 15:25:53 -0500

    Second Commit: GDD: The main.py program retrieves data science job listings from several job board RSS feeds: Indeed, ai-jobs.net, CareerCast IT & Engineering, Open Data Science Job Portal, Jobs for R-Users, MLconf Job Board, and Python Job Board. The results are date/time stamped and saved to .csv so that the program can be run at least a couple times a day, then the results can be loaded into a SQL database later (we'll avoid data duplication in the SQL database, so it's fine if the .csv files do contain duplicates, and I expect they will). The first results .csv from running the main.py program is included in this commit.

---
+++ main.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 12:41:20 2023
+
+@author: glend
+"""
+import config
+import requests as req
+from requests.auth import HTTPBasicAuth
+import time
+import pandas as pd
+
+lt = time.localtime()
+cur_sec = lt[5]
+cur_min = lt[4]
+cur_hr = lt[3]
+cur_day = lt[2]
+cur_mon = lt[1]
+cur_yr = lt[0]
+cur_date = str(cur_yr) + "-" + str(cur_mon) + "-" + str(cur_day)
+cur_time = str(cur_hr) + "-" + str(cur_min) + "-" + str(cur_sec)
+
+feeds_dict = {"Indeed": "2593879",
+              "ai-jobs.net": "2594160",
+              "CareerCastIT&Engineering": "2594162",
+              "OpenDataScienceJobPortal": "2594166",
+              "JobsforR-Users": "1378327",
+              "MLconfJobBoard": "2594174",
+              "PythonJobBoard": "977141"}
+
+feeds = ["2593879",
+         "2594160",
+         "2594162",
+         "2594166",
+         "1378327",
+         "2594174",
+         "977141"]
+
+responses = []
+
+#Feedbin
+def call_feedbin_api():
+    start = "https://api.feedbin.com/v2/feeds/"
+    end = "/entries.json"
+    for i in range(len(feeds)):
+        feed = feeds[i]
+        url = start + feed + end
+        response = req.get(url, auth = HTTPBasicAuth(config.feedbin_user, config.feedbin_pw)).json()
+        df = pd.DataFrame(data=response)
+        responses.append(df)
+    big_df = pd.concat(responses)
+    csv_str = cur_date + "_feeds_" + cur_time + ".csv"
+    big_df.to_csv(csv_str)
+
+call_feedbin_api()
